380dd4224212c549bfdc3948bbbc4f599d59440a
Priscila Ribeiro Monteiro <pririmonteiro@Priscilas-MBP.attlocal.net>
2021-06-08 12:06:10 -0700

    Completed task1 d and task 3

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ Do the following:
    HINT: no function required
 */
 
+let votingAge = 19;
+console.log (votingAge >= 18);
 
 
 /*
@@ -58,10 +60,10 @@ Do the following:
    3. Multiply a and b and return the answer
 */
 
-function multiply(/*add your code here*/){
-    /*add your code here*/
+function multiply(a, b){
+    return a *b;
   }
-
+console.log (multiply(1,3));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -107,8 +109,24 @@ Use the hungryDog function and feeding requirements below to do the following:
   NOTE: If done correctly, a weight of 15 lbs and age of 1 year would return 0.44999999999999996
 */  
 
-function hungryDog(/*add your code here*/){
-    /*add your code here*/
+function hungryDog(weight, age){
+    if(age >= 1 && weight <= 5){
+      return weight * 0.05;
+    } else if(age >=1 && weight >=6 && weight <= 10){
+      return weight * 0.04;
+    }else if (age >= 1 && weight >= 11 && weight <= 15){
+      return weight * 0.03;
+    }else if (age >=1 && weight > 15){
+      return weight * 0.02;
+    }else if(age < 1 && age >= 0.583){
+      return weight * 0.04;
+    }else if(age < 0.583 && age >= 0.333){
+      return weight * 0.05;
+    }else if(age < 0.333){
+      return weight * 0,10;
+    }else{
+      return 'please try again';
+    }
   }
 
 
